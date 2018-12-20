7a10f06fdca9721ba1cc875086b97bce0f3022d8
Anthony Rodrigues <rodrig43@elvix2.rowan.edu>
2018-12-19 23:57:28 -0500

    Crudely added Nav Bar, but it all works

--- jobFinder.php
+++ jobFinder.php
@@ -19,10 +19,45 @@ $(document).ready(function(){
   });
 });
 </script>
-  
+
+
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: white;
+            text-align: center;
+            padding: 14px 16px;
+            text-decoration: none;
+        }
+
+        /* Change the link color to #111 (black) on hover */
+        li a:hover {
+            background-color: #055857;
+        }
+    </style>
+
 
 </head>
 <body>
+
+<ul>
+    <li><a class="active" href="http://elvis.rowan.edu/~rodrig43/Final/Career_Finder_Index.php">Home</a></li>
+    <!-- <li><a href="#about">About</a></li> -->
+</ul>
+
+
 
 <div class="job-image">
   

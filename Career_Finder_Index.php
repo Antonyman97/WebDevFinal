0108a5fd8a5347901caf81a2a7f53aa5f19ebaad
Rodrigues <rodrig43@elvix2.rowan.edu>
2018-12-19 22:51:34 -0500

    inital commit

---
+++ Career_Finder_Index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+  <title>Job Finder</title>
+  <meta charset="utf-8" />
+  <meta name="Author" content="Anthony Rodrigues" />
+  <meta name="generator" content="Notepad++" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="style.css">
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script>
+$(document).ready(function(){
+  $("button").click(function(){
+   $("#hero-text").fadeOut();
+    $("#form-text").fadeIn();
+
+  });
+});
+</script>
+  
+
+</head>
+<body>
+
+<div class="hero-image">
+  <div id="hero-text">
+    <h1 style="font-size:50px">Find a Job thats right for you!</h1>
+    <button>Try it</button>
+  </div>
+  
+  <div id="form-text">
+	<h1> What do you want in a job?</h1>
+	
+  
+	<table>
+	<tr>
+		<td>Use the dropdown to select a STEM Major:</td>
+		<td><?php 
+
+# jobForm.php
+
+# M Henning
+
+require_once('debughelp.php');
+require_once('Connect.php');
+
+echo "<!--\n";
+
+echo $_SERVER['QUERY_STRING'] . "\n";
+
+echo "-->\n";
+
+$dbh = ConnectDB();
+
+echo "<form action='jobFinder.php'>\n";
+
+try {
+	$query = "select major_id, title, code from majors;";
+	$stmt = $dbh->prepare($query);
+	$stmt->execute();
+	$major_data = $stmt->fetchAll(PDO::FETCH_OBJ);
+	$stmt = null;
+}
+catch(PDOException $e)
+{
+	die ('PDO error fetching major": ' . $e->getMessage());
+}
+
+#start of the MAJOR select dropdown, tag for adding to list
+echo "<select name='major'>\n";
+
+#puts an option at the top of the dropdown
+echo "<option value='-1'>Select a Major...</option>";
+
+foreach ($major_data as $majorinfo) {
+    echo "<option value='" . $majorinfo->major_id . "'> 
+		 $majorinfo->title $majorinfo->code </option>\n";
+} 
+
+echo "</select>\n";
+
+
+#header('Location: ./majorForm.html');
+
+?></td>	
+	</tr>
+	<tr>
+		<td>Hourly or Salary?</td>
+		<td><input type="radio" name="payment" id="hourly" value="1" title="Hourly"/>  <label for="hourly">Hourly </label>
+		<input type="radio" name="payment" id="salary" value="2" title="Salary"/>  <label for="salary">Salary </label></td>
+	</tr>
+	<tr>
+		<td>Do you like working with other people?</td>
+		<td><input type="radio" name="people" id="people_yes" value="1" title="Yes"/>  <label for="people_yes">Yes </label>
+		<input type="radio" name="people" id="people_no" value="2" title="No"/>  <label for="people_no">No </label></td>
+	</tr>
+	<tr>
+		<td>Do you like to travel?</td>
+		<td><input type="radio" name="travel" id="travel_yes" value="1" title="Yes"/>  <label for="travel_yes">Yes </label>
+		<input type="radio" name="travel" id="travel_no" value="2" title="No"/>  <label for="travel_no">No </label></td>
+	</tr>
+	<tr>
+		<td>Are you ok with working on weekends?</td>
+		<td><input type="radio" name="weekends" id="weekends_yes" value="1" title="Yes"/>  <label for="weekends_yes">Yes </label>
+		<input type="radio" name="weekends" id="weekends_no" value="2" title="No"/>  <label for="weekends_no">No </label></td>
+	</tr>
+</table>
+
+<input type='submit' value="Find your Job!"/>
+
+</form>
+
+  
+  </div>
+  
+  
+  
+</div>
+
+
+
+
+
+<div class = "footer">
+ <a href="http://elvis.rowan.edu/~Rodrig43/" 
+    title="Link to my home page">
+    A. Rodrigues
+ </a>
+
+<span style="float: right;">
+
+
+    <a href="http://validator.w3.org/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://www.w3.org/Icons/valid-xhtml11"
+            alt="Valid CSS!" />
+    </a>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+    </a>
+</span>
+</div>
+
+</body>
+
+
+</html>
---
+++ jobFinder.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
+<head>
+  <title>Job Results</title>
+  <meta charset="utf-8" />
+  <meta name="Author" content="Anthony Rodrigues" />
+  <meta name="generator" content="Notepad++" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="style.css">
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ <script>
+$(document).ready(function(){
+  $("button").click(function(){
+   $("#hero-text").fadeOut();
+    $("#form-text").fadeIn();
+
+  });
+});
+</script>
+  
+
+</head>
+<body>
+
+<div class="hero-image">
+  
+  <div id="found-text">
+	
+  
+<table>
+	<tr>
+		<?php 
+
+# jobFinder.php
+# Takes input from jobForm.php and uses it to display jobs 
+# M Henning
+
+require_once('debughelp.php');
+require_once('Connect.php');
+
+echo "<!--\n";
+
+echo $_SERVER['QUERY_STRING'] . "\n";
+
+echo "-->\n";
+
+#Check if any entry is blank
+if( EMPTY(($_REQUEST['major']))){ 
+
+    die("No input for Major.");
+}
+
+if( EMPTY(($_REQUEST['payment']))){ 
+
+    die("No input for Payment.");
+}
+
+if( EMPTY(($_REQUEST['people']))){ 
+
+    die("No input for working with others.");
+}
+
+if( EMPTY(($_REQUEST['travel']))){ 
+
+    die("No input for Travel preference.");
+}
+
+if( EMPTY(($_REQUEST['weekends']))){
+
+    die("No input for Weekend preference.");
+}
+
+
+# --- ----
+echo "<tr>Your Test Answers:</tr>";
+
+$dbh = ConnectDB();
+
+# Get Major from Form
+try {
+    if ( $_REQUEST['major'] != -1){
+        $query = "select code, title from majors where major_id = :major";
+        $stmt=$dbh->prepare($query);
+        $stmt->bindValue(':major', $_REQUEST['major']);
+        $stmt->execute();
+		$major_data = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $stmt = null;
+    }
+}
+catch(PDOException $e)
+{
+    die ('PDO error adding major": ' . $e->getMessage() );
+}
+
+echo "<td>\n";
+
+foreach ($major_data as $majorinfo) {
+    echo "Major: ", $majorinfo->title, ' ', $majorinfo->code,
+		 "\n";
+	}
+
+	
+echo "</td></tr>\n";
+
+# Show payment
+if ( $_REQUEST['payment'] == 1)
+{
+echo "<tr>
+		<td>Payment: Hourly<td>
+	 </tr>";
+	 
+echo " ";
+} else{
+	
+	echo "<tr>
+		<td>Payment: Salary<td>
+	 </tr>";
+}
+
+#Show People
+
+if ( $_REQUEST['people'] == 1)
+{
+echo "<tr>
+		<td>Likes to work with people.<td>
+	 </tr>";
+	 
+echo " ";
+} else{
+	
+echo "<tr>
+		<td>Doesn't like to work with people<td>
+	 </tr>";
+
+}
+
+# Display Travel
+
+if ( $_REQUEST['travel'] == 1)
+{
+echo "<tr>
+		<td>Likes to travel.<td>
+	 </tr>";
+	 
+echo " ";
+} else {
+	
+echo "<tr>
+		<td>Doesn't like to travel.<td>
+	 </tr>";
+	 
+echo " ";
+	
+}
+
+# Display weekends
+
+if ( $_REQUEST['weekends'] == 1)
+{
+echo "<tr>
+		<td>Will work on weekends.<td>
+	 </tr>";
+	 
+echo " ";
+} else {
+	
+echo "<tr>
+		<td>Would prefer to not work weekends.<td>
+	 </tr>";
+	 
+echo " ";
+
+}
+
+# HOURLY 1
+# SALARY 2
+
+# YES 1 
+# NO 2
+
+# DISPLAY JOB INFORMATION BASED ON USER INPUT
+
+try {
+    if ( $_REQUEST['major'] != -1){
+        $query = "select major_id, jobtitle, website, Location, req, company from jobs where major_id = :major and " .
+				  "payment = :payment and people = :people and travel = :travel and weekends = :weekends";
+        $stmt=$dbh->prepare($query);
+        $stmt->bindValue(':major', $_REQUEST['major']);
+		$stmt->bindValue(':payment', $_REQUEST['payment']);
+		$stmt->bindValue(':people', $_REQUEST['people']);
+		$stmt->bindValue(':travel', $_REQUEST['travel']);
+		$stmt->bindValue(':weekends', $_REQUEST['weekends']);
+        $stmt->execute();
+		$job_data = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $stmt = null;
+    }
+}
+catch(PDOException $e)
+{
+    die ('PDO error displaying job": ' . $e->getMessage() );
+}
+
+foreach ($job_data as $jobinfo) {
+	
+	echo "<td>";
+    echo 'Suggested Job: ', $jobinfo->jobtitle, ' at ', $jobinfo->company, ' in ', $jobinfo->Location;
+	echo "</td></tr>\n";
+	echo "<tr>
+			<td>", 'School Requirement: ', $jobinfo->req, ' ', "<a href='$jobinfo->website'>More info...</a>";
+	echo "</td></tr>";
+	echo "<tr></tr>";
+	}
+	
+# CHECK IF ANY JOB MATCHED
+
+if ( EMPTY($job_data)){
+	
+	echo "<tr>";
+	echo "<td>No jobs were matched. Here are some suggestions:</td>";
+	echo "</tr>";
+	
+	try {
+    if ( $_REQUEST['major'] != -1){
+        $query = "select major_id, jobtitle, Location, website, req, company from jobs where major_id = :major";
+        $stmt=$dbh->prepare($query);
+        $stmt->bindValue(':major', $_REQUEST['major']);
+        $stmt->execute();
+		$job_data = $stmt->fetchAll(PDO::FETCH_OBJ);
+        $stmt = null;
+    }
+}
+catch(PDOException $e)
+{
+    die ('PDO error displaying job: ' . $e->getMessage() );
+}
+
+foreach ($job_data as $jobinfo) {
+	
+	echo "<tr>";
+    echo '<td>', $jobinfo->jobtitle, ' at ', $jobinfo->company, ' in ', $jobinfo->Location, ', ', 
+		 'Requirement: ', $jobinfo->req, ' ', "<a href='$jobinfo->website'>More info...</a>", '</td>';
+	echo "</tr>";
+	echo "<tr></tr>";
+}
+
+}
+
+# -- ID = 2 BIOINFORMATICS --
+# -- ID = 3 BIOLOGICAL SCIENCE --
+# -- ID = 4 BIOPHYSICS --
+# -- ID = 5 CHEMISTRY --
+# -- ID = 6 COMPUTER SCIENCE --
+# -- ID = 7 COMPUTING AND INFORMATICS --
+# -- ID = 8 MATHEMATICS --
+# -- ID = 9 PHYSICS --
+# -- ID = 10 PSYCHOLOGY --
+
+
+#header('Location: ./jobDisplay.html');
+
+
+
+?>
+  
+    </tr></table></div>
+
+
+
+</div>
+
+
+
+<div class = "footer">
+ <a href="http://elvis.rowan.edu/~Rodrig43/" 
+    title="Link to my home page">
+    A. Rodrigues
+ </a>
+
+<span style="float: right;">
+
+
+    <a href="http://validator.w3.org/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://www.w3.org/Icons/valid-xhtml11"
+            alt="Valid CSS!" />
+    </a>
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!" />
+    </a>
+</span>
+</div>
+
+
+
+
+
+</body>
+
+
+</html>
